80cea6973784ad535ffcaafc7c5b105ca0e0df61
Ayushman <ayushv2305@gmail.com>
2022-08-20 22:39:26 +0530

    ajsk

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ const SERVER_URL = 'https://scanner-backend.herokuapp.com/index/';
 const videoConstraints = {
   width: 1920,
   height: 1080,
-  facingMode: { exact: "environment" },
-  // facingMode: 'user',
+  // facingMode: { exact: "environment" },
+  facingMode: 'user',
 };
 
 function App() {
